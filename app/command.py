93db4cf30f51fe3b3cf848d73ebaa8310741b8fb
Greg Kuwaye <greg.kuwaye@gmail.com>
2020-01-12 14:21:22 -1000

    Allow command to be imported by other app modules

---
+++ app/command.py
@@ -0,0 +1,66 @@
+import cmd
+from dataclasses import asdict
+from time import sleep
+
+from tabulate import tabulate
+
+from app.services import thebus_service
+
+
+class ServerCommands(cmd.Cmd):
+    """Fetches TheBus API data and loads it into Redis"""
+    prompt = '(command) '
+
+    def do_vehicles(self, arg: str) -> None:
+        """Get all vehicle info"""
+        vehicles = [asdict(v) for v in thebus_service.get_vehicles()]
+        print(f'Found {len(vehicles)} vehicles')
+        if len(vehicles):
+            print(tabulate(vehicles, headers='keys'))
+
+    def do_active_vehicles(self, arg: str) -> None:
+        """Get info for active vehicles only"""
+        vehicles = [asdict(v) for v in thebus_service.get_active_vehicles()]
+        print(f'Found {len(vehicles)} active vehicles')
+        if len(vehicles):
+            print(tabulate(vehicles, headers='keys'))
+
+    def do_routes(self, route: str) -> None:
+        """Get info on a particular bus route"""
+        if not route:
+            print('Please enter a route')
+            return
+
+        routes = [asdict(r) for r in thebus_service.get_routes(route)]
+        print(f'Found {len(routes)} routes for route {route}')
+        if len(routes):
+            print(tabulate(routes, headers='keys'))
+
+    def do_arrivals(self, stop_id: str) -> None:
+        """Get arrival times by stop_id"""
+        try:
+            stop_id_int = int(stop_id)
+        except ValueError:
+            print('Please enter a valid numeric stop id')
+            return
+
+        arrivals = [asdict(a) for a in thebus_service.get_arrivals(stop_id_int)]
+        print(f'Found {len(arrivals)} arrivals for stop {stop_id_int}')
+        if len(arrivals):
+            print(tabulate(arrivals, headers='keys'))
+
+    def do_combined(self, arg: str) -> None:
+        self.cmdqueue.extend([
+            'hello a',
+            'hello b',
+            'delay 1.5',
+            'hello c',
+        ])
+
+    def do_delay(self, arg: str) -> None:
+        try:
+            seconds = float(arg)
+            print(f'Sleeping {seconds}s...')
+            sleep(seconds)
+        except ValueError:
+            print('Please provide a float value')
--- command.py
+++ command.py
@@ -1,68 +1,9 @@
-import cmd
-from dataclasses import asdict
-from time import sleep
+"""
+Run an interactive shell via `python command.py`.
 
-from tabulate import tabulate
-
-from app.services import thebus_service
-
-
-class ServerCommands(cmd.Cmd):
-    """Fetches TheBus API data and loads it into Redis"""
-    prompt = '(command) '
-
-    def do_vehicles(self, arg: str) -> None:
-        """Get all vehicle info"""
-        vehicles = [asdict(v) for v in thebus_service.get_vehicles()]
-        print(f'Found {len(vehicles)} vehicles')
-        if len(vehicles):
-            print(tabulate(vehicles, headers='keys'))
-
-    def do_active_vehicles(self, arg: str) -> None:
-        """Get info for active vehicles only"""
-        vehicles = [asdict(v) for v in thebus_service.get_active_vehicles()]
-        print(f'Found {len(vehicles)} active vehicles')
-        if len(vehicles):
-            print(tabulate(vehicles, headers='keys'))
-
-    def do_routes(self, route: str) -> None:
-        if not route:
-            print('Please enter a route')
-            return
-
-        routes = [asdict(r) for r in thebus_service.get_routes(route)]
-        print(f'Found {len(routes)} routes for route {route}')
-        if len(routes):
-            print(tabulate(routes, headers='keys'))
-
-    def do_arrivals(self, stop_id: str) -> None:
-        """Get arrival times by stop_id"""
-        try:
-            stop_id_int = int(stop_id)
-        except ValueError:
-            print('Please enter a valid numeric stop id')
-            return
-
-        arrivals = [asdict(a) for a in thebus_service.get_arrivals(stop_id_int)]
-        print(f'Found {len(arrivals)} arrivals for stop {stop_id_int}')
-        if len(arrivals):
-            print(tabulate(arrivals, headers='keys'))
-
-    def do_combined(self, arg: str) -> None:
-        self.cmdqueue.extend([
-            'hello a',
-            'hello b',
-            'delay 1.5',
-            'hello c',
-        ])
-
-    def do_delay(self, arg: str) -> None:
-        try:
-            seconds = float(arg)
-            print(f'Sleeping {seconds}s...')
-            sleep(seconds)
-        except ValueError:
-            print('Please provide a float value')
+The command itself is in the app.command module so that other modules inside app can use it.
+"""
+from app.command import ServerCommands
 
 
 if __name__ == '__main__':
